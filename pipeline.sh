4fe0b8c29a8bdfe7abf3de294801eaf656e6fe3a
hi-artem <aakatev@virtru.com>
2021-10-12 17:32:34 -0700

    Update pipeline.sh

--- pipeline.sh
+++ pipeline.sh
@@ -13,22 +13,28 @@ agents:
 agent_queue: &agent_queue "queue=${BUILDKITE_QUEUE}"
 
 steps:
-  - label: "Test 'echo'"
-    command: "echo 'Hello BuildKite'"
+  - label: "Workload 1"
+    command: "echo 'Executing workload 1' && sleep 100 && echo 'Done with workload 1'"
     agents:
       - *agent_queue
 
-  - wait
-
-  - label: "List '/usr/bin'"
-    command: "ls /usr/bin"
+  - label: "Workload 2"
+    command: "echo 'Executing workload 2' && sleep 100 && echo 'Done with workload 2'"
     agents:
       - *agent_queue
-
-  - wait
-
-  - label: "Test aws cli"
-    command: "aws sts get-caller-identity | jq .Arn"
+  
+  - label: "Workload 3"
+    command: "echo 'Executing workload 3' && sleep 100 && echo 'Done with workload 3'"
+    agents:
+      - *agent_queue
+  
+  - label: "Workload 4"
+    command: "echo 'Executing workload 4' && sleep 100 && echo 'Done with workload 4'"
+    agents:
+      - *agent_queue
+  
+  - label: "Workload 5"
+    command: "echo 'Executing workload 5' && sleep 100 && echo 'Done with workload 5'"
     agents:
       - *agent_queue
 YML
