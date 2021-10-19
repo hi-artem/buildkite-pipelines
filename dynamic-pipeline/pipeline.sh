d04e1f0aad63161d109ed4218d139df6847d7efa
hi-artem <aakatev@virtru.com>
2021-10-18 18:17:01 -0700

    update pipelines

---
+++ dynamic-pipeline/pipeline.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -euo pipefail
+
+if [[ -z "${LIST_TO_PRINT:-}" ]]; then
+  LIST_TO_PRINT=":github:"
+fi
+
+echo "Steps:"
+
+for item in $LIST_TO_PRINT; do
+    echo "  - label: 'Printing ${item}'"
+    echo "    command: echo '${item}'"
+done
